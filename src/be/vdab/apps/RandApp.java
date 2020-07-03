9cc17571d6fab5f6dd5148f585970d20ca7ff1d9
Marc J <john.doe.gmail.com>
2020-07-03 13:19:55 +0200

    App

---
+++ src/be/vdab/apps/RandApp.java
@@ -0,0 +1,13 @@
+package be.vdab.apps;
+
+import be.vdab.model.RandGetal;
+
+public class RandApp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25; i++) {
+            RandGetal rndGet = new RandGetal();
+            System.out.println(rndGet.getWaarde());
+            System.out.println(rndGet.getTekst());
+        }
+    }
+}
